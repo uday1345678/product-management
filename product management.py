214d0e2d8063e6bb418ea62d189b01e9425564cd
uday1345678 <udaywarring29@gmail.com>
2024-07-14 13:06:27 +0530

    productlsiting

--- product management.py
+++ product management.py
@@ -1,2 +1,31 @@
-while(True):
-    print("productmanagement")
+class productmanagement:
+    def __init__(self):
+        print("productmanagement")
+        self.product=["shirt","watch"]
+        self.productrate=[2900,3000000]
+        self.productquanity=[1,2]
+    def productlisiting(self):
+        p=input("enter product name")
+        p1=int(input("enter quantity"))
+        product=[]
+        quantity=[]
+        if(p in self.product):
+            product.append(p)
+            quantity.append(p1)
+        print(product,quantity)
+     
+
+
+
+class user:
+    def __init__(self):
+        userlist=["uday@gmail.com","uday234@gmai.com"]
+        user2=input("enter email")
+        if(user2 in userlist):
+            print("login")
+            object=productmanagement()
+            object.productlisiting()
+
+        else:
+            print("login denied")
+object=user()
